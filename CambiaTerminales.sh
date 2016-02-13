48751f08bb092033fc27d9b49cbbef6938d1c218
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-02-13 13:30:28 +0100

    resizing image done

--- CambiaTerminales.sh
+++ CambiaTerminales.sh
@@ -4,9 +4,14 @@
 #do echo "${FILES[RANDOM % ${#files[@]}]}";
 ROUTE=/home/cris/mrcrstnherediagmez@gmail.com/HD/*
 FILE=$(shuf -n 1 -e $ROUTE)
-echo $FILE.$1
+echo $FILE
+FILECONV=$(basename "$FILE")
+echo $FILECONV
+convert "$FILE" -resize %70 "/var/tmp/$FILECONV" 
+echo $FILECONV
+mv "$FILECONV" "/var/tmp/$FILECONV"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_custom_default_size --type bool "true"
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "$FILE"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "/var/tmp/$FILECONV"
 
 #gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "/home/cris/mrcrstnherediagmez@gmail.com/HD/[1600x900] - wallpaper_619186188.jpg"
 
