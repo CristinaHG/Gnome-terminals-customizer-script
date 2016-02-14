913062bd60e50027b2b4d3198136fa6629392895
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-02-15 00:30:57 +0100

    changing desktop backgroung img for gnome3

--- CambiaTerminales.sh
+++ CambiaTerminales.sh
@@ -5,15 +5,18 @@
 ROUTE=/home/cris/mrcrstnherediagmez@gmail.com/HD/*
 FILE=$(shuf -n 1 -e $ROUTE)
 echo $FILE
+FILED=$(shuf -n 1 -e $ROUTE)
+echo $FILED
 FILECONV=$(basename "$FILE")
 echo $FILECONV
-convert "$FILE" -resize %70 "/var/tmp/$FILECONV" 
+convert "$FILE" -resize %50 "/var/tmp/$FILECONV" 
 echo $FILECONV
 mv "$FILECONV" "/var/tmp/$FILECONV"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_custom_default_size --type bool "true"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "/var/tmp/$FILECONV"
-
-#gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "/home/cris/mrcrstnherediagmez@gmail.com/HD/[1600x900] - wallpaper_619186188.jpg"
+#comprobar version gnome
+#gconftool-2 -t --set /desktop/gnome/background --type string "/var/tmp/$FILED"
+gsettings set org.gnome.desktop.background picture-uri "file:///$FILED"
 
 #done
 
