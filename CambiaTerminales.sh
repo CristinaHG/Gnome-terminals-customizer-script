8b5c38faf27b393bc1cd7d92e71de63f305718a7
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-03-15 12:14:52 +0100

    searching for more colors

--- CambiaTerminales.sh
+++ CambiaTerminales.sh
@@ -20,8 +20,7 @@ gsettings set org.gnome.desktop.background picture-uri "file:///$FILED"
 
 #get hibstogram of colours from terminal Image
 
-
-convert "/var/tmp/$FILECONV" -colors 16 -depth 8 -format '%c' histogram:info:- \
+convert "/var/tmp/$FILECONV" -colors 20 -depth 8 -format '%c' histogram:info:- \
     | sort --numeric-sort \
     | gawk 'match ($0, /^ *[0-9]+: \([^)]+\) (#[0-9A-F]+) .+$/, a) { print a[1] }' \
     | tee "paleta.txt" >/dev/null
