5ecaeb90250422bc6f9bcefe34d89844b0861648
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-03-21 16:38:24 +0100

    final

--- CambiaTerminales.sh
+++ CambiaTerminales.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
-#FILES=(/home/cris/mrcrstnherediagmez@gmail.com/HD/*)
-#for f in $FILES;
-#do echo "${FILES[RANDOM % ${#files[@]}]}";
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+export DISPLAY=:0.0
+
 ROUTE=/home/cris/mrcrstnherediagmez@gmail.com/HD/*
 FILE=$(shuf -n 1 -e $ROUTE)
-echo $FILE
+#echo $FILE
 FILED=$(shuf -n 1 -e $ROUTE)
-echo $FILED
+#echo $FILED
 FILECONV=$(basename "$FILE")
-echo $FILECONV
+#echo $FILECONV
 convert "$FILE" -resize %50 "/var/tmp/$FILECONV" 
-echo $FILECONV
-mv "$FILECONV" "/var/tmp/$FILECONV"
+#echo $FILECONV
+#mv "$FILECONV" "/var/tmp/$FILECONV" > /dev/null
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_custom_default_size --type bool "true"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_image --type string "/var/tmp/$FILECONV"
 #comprobar version gnome
 #gconftool-2 -t --set /desktop/gnome/background --type string "$FILED"
+
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_darkness --type float "0.78"
+ 
 gsettings set org.gnome.desktop.background picture-uri "file:///$FILED"
 
 #get hibstogram of colours from terminal Image
 
-convert "/var/tmp/$FILECONV" -colors 20 -depth 8 -format '%c' histogram:info:- \
+convert "/var/tmp/$FILECONV" -colors 25 -depth 6 -format '%c' histogram:info:- \
     | sort --numeric-sort \
     | gawk 'match ($0, /^ *[0-9]+: \([^)]+\) (#[0-9A-F]+) .+$/, a) { print a[1] }' \
-    | tee "paleta.txt" >/dev/null
-  #  | while read colour; do convert -size 20x20 "xc:$colour" +depth miff:-; done \
- #   | montage - -geometry +0+0 "palette.png"
+    | tee "/var/tmp/paleta.txt" >/dev/null
 
-exec 3< paleta.txt
+exec 3< /var/tmp/paleta.txt
 read color1 <&3
 read color2 <&3
 read color3 <&3
-echo "Four lines: $color1 $color2 $color3"
+read color4 <&3
+read color5 <&3
+read color6 <&3
 exec 3<&-
 
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "$color1"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color --type string "$color6"
+#gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color --type string "$color4"
+#export PS1
+#PS1='${debian_chrootoot)}\[\033[1;32m\]\u@\h\[\e[1;#2C514D\a]:\[\033[01;34m\]\w\[\033[1;37m\]\$ '
+
 
 #done
 
