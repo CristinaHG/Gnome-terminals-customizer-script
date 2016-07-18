0f9d320015198008805d88a51f7412b4f3bf2e91
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-07-18 18:34:51 +0200

    cleaning code

--- CambiaTerminales.sh
+++ CambiaTerminales.sh
@@ -10,11 +10,12 @@ GCONFT="/usr/bin/gconftool-2"
 SORT="/usr/bin/sort"
 GAWK="/usr/bin/gawk"
 TEE="/usr/bin/tee"
+CNVRT="/usr/bin/convert"
 ROUTE="/home/cris/mrcrstnherediagmez@gmail.com/HD/*"
 
 #toma aleatoriamente una imágen para la terminal
 FILE=$(shuf -n 1 -e $ROUTE)
-#toma aleatoriamente otra imágen para el desktop 
+#toma aleatoriamente otra imágen para el desktop (opcional)
 FILED=$(shuf -n 1 -e $ROUTE)
 #limpiamos la ruta, nos quedamos solo con el nombre de la imágen
 FILECONV=$(basename "$FILE")
@@ -29,7 +30,7 @@ convert "$FILE" -resize %50 "/tmp/$FILECONV"
 
 #configuraciones antes de aplicar los cambios para que tengan efecto
 $GCONFT --set /apps/gnome-terminal/profiles/Default/background_type --type string "image" #solid por defecto
-$GCONFT --set /apps/gnome-terminal/profiles/Default/use_custom_command --type bool "true" #false por defecto
+$GCONFT --set /apps/gnome-terminal/profiles/Default/use_custom_command --type bool "false" 
 $GCONFT --set /apps/gnome-terminal/profiles/Default/use_theme_colors --type bool "false"  #true por defecto
 $GCONFT --set /apps/gnome-terminal/profiles/Default/use_custom_default_size --type bool "true"
 #tomamos la imágen redimensionada y la ponemos como imágen de fondo del terminal
@@ -44,13 +45,12 @@ $GCONFT --set /apps/gnome-terminal/profiles/Default/background_darkness --type f
 #hay que poner un color de letra que vaya con los tonos de la imágen, pero que se aprecien bien.
 #Para ello:
 #1) sacamos el hibstograma de colores de la imágen 
-#2) los ordenamos por valor numérico del string
+#2) los ordenamos por valor numérico 
 #3) filtramos con un patrón solo los código hexadecimal de los colores
 #4) escribe valores hexadecimales en fichero de salida
-/usr/bin/convert "/tmp/$FILECONV" -colors 25 -depth 6 -format '%c' histogram:info:- \
-    | $SORT --numeric-sort \
-    | $GAWK 'match ($0, /^ *[0-9]+: \([^)]+\) (#[0-9A-F]+) .+$/, a) { print a[1] }' \
-    | $TEE "/tmp/paleta.txt" >/dev/null
+$CNVRT "/tmp/$FILECONV" -colors 25 -depth 6 -format '%c' histogram:info:- \
+    | $SORT --general-numeric-sort \
+    | $GAWK 'match ($0, /^ *[0-9]+: \([^)]+\) (#[0-9A-F]+) .+$/, a) { print a[1] }' > "/tmp/paleta.txt" #>/dev/null
 
 #abrimos el fichero
 exec 3< /tmp/paleta.txt
